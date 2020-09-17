fd61d8b77c3c0f3d01cdd83f350b85bab5195460
Mike Mwanje <mwanjemike767@gmail.com>
2020-09-17 23:47:41 +0300

    Transfer notification

--- public/script.js
+++ public/script.js
@@ -1,14 +1,29 @@
-  let bankpayform=document.getElementById('bankpay')
-  let selectpaybutton=document.getElementById('selectpay')
-  let banktransferbtn=document.getElementById('bank-transfer-btn')
-  selectpaybutton.onclick=function(){
-  bankpayform.style.display='block'
+let bankpayform = document.getElementById("bankpay");
+let selectpaybutton = document.getElementById("selectpay");
+let banktransferbtn = document.getElementById("bank-transfer-btn");
+// selectpaybutton.onclick = function () {
+//   bankpayform.style.display = "block";
+// };
+banktransferbtn.onclick = function () {
+  bankpayform.style.display = "none";
+};
+
+// banking notification
+function bnotification() {
+  // let bank=document.getElementById('bank').value;
+  let amount = document.getElementById("bamount").value;
+  //   alert(`transfer shs ${amount} from ${bank}`)
+  if (amount.length !== "") {
+    confirm(`You are about to make a deposit of ${amount}, Are you sure?`);
   }
-  banktransferbtn.onclick=function(){
-  bankpayform.style.display='none'
+}
+
+// mobile money notification
+function mnotification() {
+  // let bank=document.getElementById('bank').value;
+  let amount = document.getElementById("mamount").value;
+  //   alert(`transfer shs ${amount} from ${bank}`)
+  if (amount.length !== "") {
+    confirm(`You are about to make a deposit of ${amount}, Are you sure?`);
   }
-  function notification (){
-  let bank=document.getElementById('bank').value;
-  let amount=document.getElementById('amount').value;
-  alert(`transfer shs ${amount}from ${bank}`)
-  }
+}
