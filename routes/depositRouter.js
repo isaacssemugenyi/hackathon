312e4bc5bbaed792fe88a137c56b26900003c2b2
Isaac Ssemugenyi <isaacpro01@gmail.com>
2020-09-17 09:37:56 +0300

    Added mojaloop linking

--- routes/depositRouter.js
+++ routes/depositRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const axios = require('axios');
 
 const isAuth = require('../config/auth');
 // const isAllowed = require('../config/access')
@@ -17,10 +18,26 @@ router.post('/', isAuth,  async(req, res)=>{
     deposit.amount = req.body.amount;
     deposit.reference = req.user.id;
     deposit.createdAt = Date.now();
-
-    try {
+    try{
+    const postReq =  await axios.post('http://teamtime.hipipo.mojaloop-hackathon.io:4301/transfers', 
+    {
+        "from": { "displayName": req.user.fullname, "idType": "MSISDN", "idValue": req.user.id },
+        "to": { "idType": "MSISDN", "idValue": "498941207269" },
+        "amountType": "SEND",
+        "currency": "EUR",
+        "amount": req.body.amount,
+        "transactionType": "TRANSFER",
+        "initiatorType": "CONSUMER",
+        "note": "test payment",
+        "homeTransactionId": "{{$guid}}"
+    })
+    if(postReq.status === 200){
         await deposit.save();
         res.redirect('/client/dashboard');
+    } else{
+        req.flash('Was unable to save data');
+        res.redirect('/client/dashboard');
+    }   
     } catch(err){
         throw new Error(err.message)
     }
